f8cb31db6cc522212e7f6ed99e34a856de57dccd
zizifn <515868058@qq.com>
2018-12-23 22:50:56 +0800

    update callback and promise

--- Callback_Promise_obser/callback2Promise.js
+++ Callback_Promise_obser/callback2Promise.js
@@ -48,6 +48,8 @@ function get(url) {
                     reject(Error("Network Error"));
                 };
 
+                var startTime = Date.now();
+                console.log("%s startTime is %s", url, startTime)
                 req.send();
             } catch (error) {
                 console.log("error is ", error);
@@ -142,31 +144,128 @@ function getJson(url) {
 
 // 对比以上code 与 callback
 
-// 下面才是展示真正的实力的时候
+// 下面才是展示真正的实力的时候.
 
-var storyPromise;
+// var storyPromise;
+// function getChapter(i){
+//     storyPromise = storyPromise || getJson('https://raw.githubusercontent.com/zizifn/thoughts/master/data/primers/story.json');
 
-function getChapter(i){
-    storyPromise = storyPromise || getJson('https://raw.githubusercontent.com/zizifn/thoughts/master/data/primers/story.json');
+//     return storyPromise.then(
+//         (storyResp) =>{
+//             return getJson("https://raw.githubusercontent.com/zizifn/thoughts/master/data/primers/" + storyResp.chapterUrls[i]);
+//         }
+//     )
+// };
 
-    return storyPromise.then(
-        (storyResp) =>{
-            return getJson("https://raw.githubusercontent.com/zizifn/thoughts/master/data/primers/" + storyResp.chapterUrls[i]);
-        }
-    )
+// getChapter(0).then(
+//     chapterJson =>{
+//         console.log("chapter json is ", chapterJson);
+//     }
+// );
+
+// //storyPromise will hold(resolved), next time, it's just return
+// getChapter(1).then(
+//     chapterJson =>{
+//         console.log("chapter2 json is ", chapterJson);
+//     }
+// );
+
+
+// error handler
+// getJson('https://raw.githubusercontent.com/zizifn/thoughts/master/data/primers/story.json').then(
+//     (response) => {
+//         console.log("Success!", response);
+//     }
+// ).catch(
+//     (error) => {
+//         console.log('Faild!', error);
+//     }
+// )
+
+// var jsonPromise = new Promise(
+//     (resolve, reject) =>{
+//         // reject is x显示reject
+
+//         //JSON.parse will throw error, so 隐士reject.. So error will auto catch to reject
+//         resolve(JSON.parse("This is not JSON"));
+//     }
+// );
+// jsonPromise.then(
+//     response => {
+//         console.log("response", response);
+//     }
+// ).catch(
+//     error => {
+//         console.log("It's failed", error);
+//     }
+// )
+
+
+//并发
+
+
+// getJson('https://raw.githubusercontent.com/zizifn/thoughts/master/data/primers/story.json').then(
+//     story => {
+//         console.log("并发!!!!");
+//         console.log("story is ", story)
+//         story.chapterUrls.forEach((element, index)  => {
+//             getJson("https://raw.githubusercontent.com/zizifn/thoughts/master/data/primers/" + element).then(
+//                 chapter =>{
+//                     console.log("chapter %s is ", index, chapter)
+//                 }
+//             )
+//         });
+//     }
+// ).then(
+//     () => {
+//         console.log("all done")
+//     }
+// ).catch(
+//     error => {
+//         console.log("error is ", error);
+//     }
+// )
+
+
+//generator
+function *addGenerator() {
+    var i = 0;
+    while (true) {
+        i += yield i;
+    }
 };
 
-getChapter(0).then(
-    chapterJson =>{
-        console.log("chapter json is ", chapterJson);
-    }
-);
-
-//storyPromise will hold(resolved), next time, it's just return
-getChapter(1).then(
-    chapterJson =>{
-        console.log("chapter2 json is ", chapterJson);
-    }
-);
+var adder = addGenerator();
+console.log(adder.next().value); // 0
+console.log(adder.next(5).value); // 5
+console.log(adder.next(5).value); // 10
+console.log(adder.next(5).value); // 15
+console.log(adder.next(50).value); // 65
 
 
+getJSON('story.json').then(function(story) {
+    addHtmlToPage(story.heading);
+
+    // Map our array of chapter urls to
+    // an array of chapter json promises.
+    // This makes sure they all download parallel.
+    return story.chapterUrls.map(getJSON)
+      .reduce(function(sequence, chapterPromise) {
+        // Use reduce to chain the promises together,
+        // adding content to the page for each chapter
+        return sequence.then(function() {
+          // Wait for everything in the sequence so far,
+          // then wait for this chapter to arrive.
+          return chapterPromise;
+        }).then(function(chapter) {
+          addHtmlToPage(chapter.html);
+        });
+      }, Promise.resolve());
+  }).then(function() {
+    addTextToPage("All done");
+  }).catch(function(err) {
+    // catch any error that happened along the way
+    addTextToPage("Argh, broken: " + err.message);
+  }).then(function() {
+    document.querySelector('.spinner').style.display = 'none';
+  })
---
+++ Callback_Promise_obser/readme.md
@@ -0,0 +1,14 @@
+
+# callback and promise
+
+* callback to promise
+* Promise
+* Paralleliism and sequencing
+  * sequencing
+    * one by one
+  * Paralleliism
+    * send them all, and control order
+    * send them all, and when all are done->process
+      * 类比小说load， load完成后，一起显示。
+    * send them all and when anyone resolve->process
+      * 类比小说load， 先回来的章节，就显示。
