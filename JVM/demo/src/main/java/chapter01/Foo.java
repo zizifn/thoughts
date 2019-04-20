047d74783f08de3dfc8beb274bac142b1de406db
zizfn <1803942+zizifn@users.noreply.github.com>
2019-04-20 19:18:51 +0800

    update project.

---
+++ JVM/demo/src/main/java/chapter01/Foo.java
@@ -0,0 +1,22 @@
+package chapter01;
+
+public class Foo {
+    public static void main(String[] args) {
+
+        boolean flag2 = false;
+
+        for(int i=0;i<100;i++){
+            flag2 = true;
+            System.out.println("Hello, Java!");
+        }
+        boolean flag = true;
+        if (flag) //IFEQ
+        {
+            System.out.println("Hello, Java!");
+        }
+        if (flag == true)//IF_ICMPNE
+        {
+            System.out.println("Hello, JVM!");
+        }
+    }
+}
