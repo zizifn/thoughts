f0e6daab2edcae37e278f72d0f53255f4f9787b5
zizifn <515868058@qq.com>
2019-11-24 22:54:40 +0800

    update files.

--- test-ng-project/src/app/app-routing.module.ts
+++ test-ng-project/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FlexBoxComponent } from './flex-box/flex-box.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'flexbox' , component: FlexBoxComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ test-ng-project/src/app/flex-box/flex-box.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flex-box',
+  templateUrl: './flex-box.component.html',
+  styleUrls: ['./flex-box.component.scss']
+})
+export class FlexBoxComponent implements OnInit {
+
+  str: string = "X 100";
+  testBol = true;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
