f1b21d2a37a3c5693eb67a21125b53cb877a1d03
zizfn <1803942+zizifn@users.noreply.github.com>
2019-11-24 22:44:50 +0800

    update druid..

---
+++ spring/jdbc/demo/src/main/java/com/james/demo/druid/ConnectionLogFilter.java
@@ -0,0 +1,25 @@
+package com.james.demo.druid;
+
+import com.alibaba.druid.filter.FilterChain;
+import com.alibaba.druid.filter.FilterEventAdapter;
+import com.alibaba.druid.proxy.jdbc.ConnectionProxy;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Properties;
+
+/**
+ * @author James
+ */
+@Slf4j
+public class ConnectionLogFilter extends FilterEventAdapter {
+
+    @Override
+    public void connection_connectBefore(FilterChain chain, Properties info) {
+        log.info("BEFORE CONNECTION!");
+    }
+
+    @Override
+    public void connection_connectAfter(ConnectionProxy connection) {
+        log.info("AFTER CONNECTION!");
+    }
+}
--- spring/jdbc/demo/src/test/java/com/james/demo/DemoApplicationTests.java
+++ spring/jdbc/demo/src/test/java/com/james/demo/DemoApplicationTests.java
@@ -1,12 +1,9 @@
 package com.james.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+//@SpringBootTest
 class DemoApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
