ba196970bfffe0e36631f6f9ef645f70b3568942
zizifn <515868058@qq.com>
2019-12-16 19:45:01 +0800

    update leetcode.

--- leetcode-java/src/main/java/com/james/leetcodejava/leetcode/editor/cn/TwoSum.java
+++ leetcode-java/src/main/java/com/james/leetcodejava/leetcode/editor/cn/TwoSum.java
@@ -15,13 +15,30 @@
   public class TwoSum{
       public static void main(String[] args) {
            Solution solution = new TwoSum().new Solution();
+
+          int[] ints = solution.twoSum(new int[]{2, 7, 11, 15}, 9);
+          ints = solution.twoSum(new int[]{3, 2, 4}, 6);
+
+          for (int i: ints
+               ) {
+              System.out.println(i);
+          }
       }
       
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        return new int[]{1, 2};
+
+        for (int i = 0; i < nums.length -1 ; i++) {
+            for (int j = i + 1; j < nums.length ; j++) {
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i, j};
+                }
+            }
+
+        }
+        return new int[] {};
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
