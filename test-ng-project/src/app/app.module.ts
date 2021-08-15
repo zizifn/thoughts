2de9ee3e3cca223cbc0cfaa09e76d9b5f06fcf25
zizifn <515868058@qq.com>
2021-08-15 15:48:03 +0800

    update file

--- test-ng-project/src/app/app.module.ts
+++ test-ng-project/src/app/app.module.ts
@@ -1,19 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ParentComponent } from './parent/parent.component';
-import { ChildComponent } from './parent/child/child.component';
-import { FlexBoxComponent } from './flex-box/flex-box.component';
-import { ViewchildTestComponent } from './viewchild-test/viewchild-test.component';
-import { HookComponent } from './hook/hook.component';
-import { HttpCallComponent } from './http-call/http-call.component';
-import { ReduxTestComponent } from './redux-test/redux-test.component';
-import { LocalStore } from './redux-test/local-store';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ParentComponent } from "./parent/parent.component";
+import { ChildComponent } from "./parent/child/child.component";
+import { FlexBoxComponent } from "./flex-box/flex-box.component";
+import { ViewchildTestComponent } from "./viewchild-test/viewchild-test.component";
+import { HookComponent } from "./hook/hook.component";
+import { HttpCallComponent } from "./http-call/http-call.component";
+import { ReduxTestComponent } from "./redux-test/redux-test.component";
+import { LocalStore } from "./redux-test/local-store";
+import { JamesWc } from "james-wc";
+// window.customElements.define("james-wc", JamesWc);
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,16 +24,12 @@ import { LocalStore } from './redux-test/local-store';
     ViewchildTestComponent,
     HookComponent,
     HttpCallComponent,
-    ReduxTestComponent
+    ReduxTestComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [LocalStore],
   entryComponents: [ViewchildTestComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
